c680d3319841515b6777e6945198487ec256bb62
user <vladislavkhamin90@gmail.com>
2025-02-08 20:04:46 +0300

    add activity, icons, xml files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.playlist_maker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.playlist_maker"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
