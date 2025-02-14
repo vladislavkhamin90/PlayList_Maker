ea67981135b385b1353ff596448b27d1ee94d5f1
user <vladislavkhamin90@gmail.com>
2025-02-14 17:58:46 +0300

    Add intents

--- app/src/main/java/com/example/playlist_maker/MainActivity.kt
+++ app/src/main/java/com/example/playlist_maker/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.playlist_maker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
--- app/src/main/java/com/example/playlist_maker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlist_maker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlist_maker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -21,6 +24,37 @@ class SettingsActivity : AppCompatActivity() {
         val tittleBackIcon = findViewById<Toolbar>(R.id.title)
         tittleBackIcon.setNavigationOnClickListener {
             this.finish()
+        }
+
+        val shareLine = findViewById<TextView>(R.id.sharing)
+        val supportLine = findViewById<TextView>(R.id.support)
+        val agreementLine = findViewById<TextView>(R.id.agreement)
+
+        shareLine.setOnClickListener{
+            val url = Uri.parse("https://practicum.yandex.ru/profile/android-developer/")
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, url)
+            startActivity(Intent.createChooser(intent, "Поделиться ссылкой"))
+        }
+
+        supportLine.setOnClickListener{
+            val intent = Intent(Intent.ACTION_SEND)
+            val title = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val text = "Спасибо разработчикам и разработчицам за крутое приложение"
+            val mail = "vladislavkhamin90@gmail.com"
+            intent.putExtra(Intent.EXTRA_EMAIL, mail)
+            intent.putExtra(Intent.EXTRA_SUBJECT, title)
+            intent.putExtra(Intent.EXTRA_TEXT, text)
+            intent.data = Uri.parse("mailto:")
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Send Email"))
+        }
+
+        agreementLine.setOnClickListener{
+            val url = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val intent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(Intent.createChooser(intent,"Открыть ссылку"))
         }
     }
 }
