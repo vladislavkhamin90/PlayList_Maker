dfef490d13a98f593c4a70b8106cacf817cb1c0c
user <vladislavkhamin90@gmail.com>
2025-02-26 20:32:33 +0300

    fix image placeholder, clear button, switch colors, class TrackViewHolder

--- app/src/main/java/com/example/playlist_maker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlist_maker/TrackAdapter.kt
@@ -1,14 +1,8 @@
 package com.example.playlist_maker
 
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.*
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 
 class TrackAdapter(private val tracks: List<Track>):
@@ -24,23 +18,4 @@ class TrackAdapter(private val tracks: List<Track>):
     }
 
     override fun getItemCount() = tracks.size
-}
-
-
-class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
-    private val nameTrack: TextView = itemView.findViewById(R.id.title_music)
-    private val artistTrack: TextView = itemView.findViewById(R.id.artist)
-    private val time: TextView = itemView.findViewById(R.id.time)
-    private val image: ImageView = itemView.findViewById(R.id.image)
-
-    fun bind(track: Track){
-        nameTrack.text = track.trackName
-        artistTrack.text = track.artistName
-        time.text = track.trackTime
-        Glide.with(itemView)
-            .load(track.artworkUrl100).transform(RoundedCorners(2))
-            .placeholder(R.drawable.placeholder)
-            .into(image)
-    }
 }
